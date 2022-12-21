d23a62f6e0b4edca99011a773c2d8de09613d18e
IsseiClockWise <isseitakeda@clockwisesystem.com>
2022-12-21 14:21:35 +0900

    first commit

---
+++ BookList/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace BookList
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナ変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナで生成されたコード
+
+        /// <summary>
+        /// デザイナ サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディタで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.dgvBook = new System.Windows.Forms.DataGridView();
+            this.No = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Title = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Price = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Author = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBook)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnOk
+            // 
+            this.btnOk.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnOk.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnOk.Location = new System.Drawing.Point(246, 392);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(122, 43);
+            this.btnOk.TabIndex = 1;
+            this.btnOk.Text = "登録";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(402, 392);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(122, 43);
+            this.btnCancel.TabIndex = 2;
+            this.btnCancel.Text = "終了";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // dgvBook
+            // 
+            this.dgvBook.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvBook.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.ControlLight;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("MS UI Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvBook.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgvBook.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvBook.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.No,
+            this.Title,
+            this.Price,
+            this.Author});
+            this.dgvBook.EnableHeadersVisualStyles = false;
+            this.dgvBook.Location = new System.Drawing.Point(14, 27);
+            this.dgvBook.Name = "dgvBook";
+            this.dgvBook.RowTemplate.Height = 24;
+            this.dgvBook.Size = new System.Drawing.Size(510, 346);
+            this.dgvBook.TabIndex = 0;
+            // 
+            // No
+            // 
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.No.DefaultCellStyle = dataGridViewCellStyle2;
+            this.No.HeaderText = "No";
+            this.No.Name = "No";
+            // 
+            // Title
+            // 
+            this.Title.HeaderText = "タイトル";
+            this.Title.Name = "Title";
+            // 
+            // Price
+            // 
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleRight;
+            this.Price.DefaultCellStyle = dataGridViewCellStyle3;
+            this.Price.HeaderText = "金額";
+            this.Price.Name = "Price";
+            // 
+            // Author
+            // 
+            this.Author.HeaderText = "作者";
+            this.Author.Name = "Author";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(536, 456);
+            this.Controls.Add(this.dgvBook);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOk);
+            this.Font = new System.Drawing.Font("MS UI Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Name = "Form1";
+            this.Text = "Book List";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBook)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.DataGridView dgvBook;
+        private System.Windows.Forms.DataGridViewTextBoxColumn No;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Title;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Price;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Author;
+    }
+}
+
---
+++ BookList/Form1.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace BookList
+{
+    public enum EnmCol
+    {
+        colNo = 0,
+        colTitle,
+        colPrice,
+        colAuthor,
+        colEnd
+    }
+
+    /// <summary>
+    /// Form1 Class
+    /// </summary>
+    public partial class Form1 : Form
+    {
+        // クラスのメンバアイテム
+        private List<BOOKDATA> ListBooks = null;
+        private const string BOOK_DATA_FILE = @"C:\Temp\BookData.csv";
+        private const int BOOK_ITEM_NUM = 4;  
+
+
+        /// <summary>
+        /// Form1 コンストラクタ
+        /// インスタンスが生成された瞬間に実行されるメソッド
+        /// </summary>
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        /// <summary>
+        /// LOAD
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // File Read
+            Read_BooksCsv(BOOK_DATA_FILE, out ListBooks);
+
+            // データをDataGridViewに設定
+            dgvBook.Rows.Clear();       // DataGridViewアイテムクリア
+            for (int i = 0; i < ListBooks.Count; i++)
+            {
+                // 行を追加
+                int r = dgvBook.Rows.Add();
+                dgvBook[(int)EnmCol.colNo, r].Value = ListBooks[i].iNo.ToString();
+                dgvBook[(int)EnmCol.colTitle, r].Value = ListBooks[i].stTitle;
+                dgvBook[(int)EnmCol.colPrice, r].Value = ListBooks[i].iPrice.ToString();
+                dgvBook[(int)EnmCol.colAuthor, r].Value = ListBooks[i].stAuthor;
+            }
+        }
+
+        
+        /// <summary>
+        /// CancelButton
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        
+        /// <summary>
+        /// OK Button
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            // ListBooks クリア
+            ListBooks = new List<BOOKDATA>();
+
+            // dgvBookの内容をList<BOOKDATA>に格納
+            if (dgvBook.Rows.Count <= 1) return;    // 1:新規行
+
+            for (int i = 0; i < dgvBook.RowCount - 1; i++)
+            {
+                BOOKDATA book = new BOOKDATA();
+                if (dgvBook[(int)EnmCol.colNo, i].Value != null
+                    && dgvBook[(int)EnmCol.colNo, i].Value.ToString().Length > 0)
+                {
+                    book.iNo = int.Parse(dgvBook[(int)EnmCol.colNo, i].Value.ToString());
+                }
+                if (dgvBook[(int)EnmCol.colTitle, i].Value != null
+                    && dgvBook[(int)EnmCol.colTitle, i].Value.ToString().Length > 0)
+                {
+                    book.stTitle = dgvBook[(int)EnmCol.colTitle, i].Value.ToString();
+                }
+                if (dgvBook[(int)EnmCol.colPrice, i].Value != null
+                    && dgvBook[(int)EnmCol.colPrice, i].Value.ToString().Length > 0)
+                {
+                    book.iPrice = int.Parse(dgvBook[(int)EnmCol.colPrice, i].Value.ToString());
+                }
+                if (dgvBook[(int)EnmCol.colAuthor, i].Value != null
+                    && dgvBook[(int)EnmCol.colAuthor, i].Value.ToString().Length > 0)
+                {
+                    book.stAuthor = dgvBook[(int)EnmCol.colAuthor, i].Value.ToString();
+                }
+
+                ListBooks.Add(book);
+            }
+
+            if (ListBooks != null && ListBooks.Count > 0)
+            {
+                if (Write_BooksCsv(BOOK_DATA_FILE, ListBooks))
+                {
+                    MessageBox.Show("書込み完了しました。",
+                                    "BookList",
+                                    MessageBoxButtons.OK,
+                                    MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("書込み失敗しました。",
+                                    "BookList",
+                                    MessageBoxButtons.OK,
+                                    MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        
+        /// <summary>
+        /// CSVファイルを読込み、lstBooksにセットする
+        /// </summary>
+        /// <param name="flName"></param>
+        /// <param name="lstBooks"></param>
+        /// <returns></returns>
+        public bool Read_BooksCsv(string flName, out List<BOOKDATA> lstBooks)
+        {
+            lstBooks = new List<BOOKDATA>();
+
+            if (!File.Exists(flName)) return false;
+
+            // File open
+            using (StreamReader sr = new StreamReader(flName, Encoding.GetEncoding("shift_jis")))
+            {
+                string read_buf;
+
+                // Read
+                while ((read_buf = sr.ReadLine()) != null)
+                {
+                    string[] items = read_buf.Split(',');
+
+                    if (items.Length < BOOK_ITEM_NUM) continue;
+
+                    BOOKDATA book = new BOOKDATA();
+                    if (items[0] != null && items[0].Length > 0)
+                    {
+                        book.iNo = int.Parse(items[0]);
+                    }
+                    if (items[1] != null && items[1].Length > 0)
+                    {
+                        book.stTitle = items[1];
+                    }
+                    if (items[2] != null && items[2].Length > 0)
+                    {
+                        book.iPrice = int.Parse(items[2]);
+                    }
+                    if (items[3] != null && items[3].Length > 0)
+                    {
+                        book.stAuthor = items[3];
+                    }
+
+                    lstBooks.Add(book);
+                }
+
+                // File close
+                sr.Close();
+            }
+
+            return true;
+        }
+
+
+        /// <summary>
+        /// lstBooksに格納されているデータをCSVファイルに書込む
+        /// </summary>
+        /// <param name="flName"></param>
+        /// <param name="lstBooks"></param>
+        /// <returns></returns>
+        public bool Write_BooksCsv(string flName, List<BOOKDATA> lstBooks)
+        {
+            if (lstBooks == null || lstBooks.Count == 0) return false;
+
+            StreamWriter sw = null;
+
+            try
+            {
+                sw = new StreamWriter(flName, false, Encoding.GetEncoding("shift_jis"));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return false;
+            }
+            foreach (BOOKDATA book in lstBooks)
+            {
+                string rec_buff = book.iNo.ToString()
+                                + ","
+                                + book.stTitle
+                                + ","
+                                + book.iPrice.ToString()
+                                + ","
+                                + book.stAuthor;
+                sw.WriteLine(rec_buff);
+            }
+            sw.Close();
+
+            return true;
+        }
+    }
+
+    
+    /// <summary>
+    /// BOOKDATA クラス
+    /// </summary>
+    public class BOOKDATA
+    {
+        public int iNo;
+        public string stTitle;
+        public int iPrice;
+        public string stAuthor;
+    }
+}
